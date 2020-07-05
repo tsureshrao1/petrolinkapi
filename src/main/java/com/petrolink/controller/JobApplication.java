3b068181b550b90aacb1a61a356d6bec4aacdcd5
svajral <svajral@XIN009CLSVAJRAL.ctl.intranet>
2020-07-05 08:52:30 +0530

    file download api added

--- src/main/java/com/petrolink/controller/JobApplication.java
+++ src/main/java/com/petrolink/controller/JobApplication.java
@@ -1,9 +1,20 @@
 package com.petrolink.controller;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -76,6 +87,71 @@ public class JobApplication {
 		}
 
 		return profiles;
+	}
+
+	@RequestMapping("/profile/download/{fileName:.+}")
+	public void downloadPDFResource(HttpServletRequest request, HttpServletResponse response,
+			@PathVariable("fileName") String fileName) throws IOException {
+
+		// fileName expected in P_2_7_ in this careerId is 2 and profileId is 7
+		try {
+			File dir = new File(filePath);
+
+			File[] matches = dir.listFiles(new FilenameFilter() {
+				public boolean accept(File dir, String name) {
+
+					return name.startsWith(fileName);
+				}
+			});
+
+			File file = null;
+
+			if (matches.length > 0) {
+				file = matches[0];
+			}
+
+			if (file == null) {
+				throw new RuntimeException("File does not found with starting with that file name  " + fileName);
+			}
+
+			if (file.exists()) {
+
+				String fullFileName = file.getName();
+				String originalFileName = fullFileName.replace(fileName, "");
+
+				System.out.println("fileName ::" + fileName);
+				System.out.println("originalFileName ::" + originalFileName);
+
+				String mimeType = URLConnection.guessContentTypeFromName(file.getName());
+
+				if (mimeType == null) {
+					// unknown mimetype so set the mimetype to application/octet-stream
+					mimeType = "application/octet-stream";
+				}
+
+				response.setContentType(mimeType);
+
+				/**
+				 * Here we have mentioned it to show inline
+				 */
+				// response.setHeader("Content-Disposition", String.format("inline; filename=\""
+				// + file.getName() + "\""));
+
+				// Here we have mentioned it to show as attachment
+				response.setHeader("Content-Disposition",
+						String.format("attachment; filename=\"" + originalFileName + "\""));
+
+				response.setContentLength((int) file.length());
+
+				InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+
+				FileCopyUtils.copy(inputStream, response.getOutputStream());
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(" Exception occured while downloading document ", e);
+		}
 	}
 
 }
