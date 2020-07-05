05a549fe158604658be813f2a913fb91be8a042d
svajral <svajral@XIN009CLSVAJRAL.ctl.intranet>
2020-07-05 20:49:09 +0530

    Muthu changes commited along with file download options

--- src/main/java/com/petrolink/PetrolinkApplication.java
+++ src/main/java/com/petrolink/PetrolinkApplication.java
@@ -2,9 +2,10 @@ package com.petrolink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PetrolinkApplication {
+public class PetrolinkApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetrolinkApplication.class, args);
